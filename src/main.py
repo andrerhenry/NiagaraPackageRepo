13c8027dbfa2eeffa1c900c49fdcbe727b34ac5b
andrerhenry <kinghenry238@gmail.com>
2025-03-28 23:15:26 -0700

    created intial get function for packages

--- src/main.py
+++ src/main.py
@@ -1,3 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/niagara/4.14/<package_name>', methods=['GET'])
+def get_package(package_name:str):
+    return print(f'Attempting to get: {package_name}')
+
+
+
 def main():
     print("Hello from niagarapackagerepo!")
 
