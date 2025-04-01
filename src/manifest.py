8b1df7d51b0ff3cd56c9ba8fdd8f2e31324ba64c
andrerhenry <kinghenry238@gmail.com>
2025-03-31 22:39:21 -0700

    imput directory list in to json

--- src/manifest.py
+++ src/manifest.py
@@ -1,5 +1,24 @@
 import os
+import json
 
 def create_module_manifest():
     list_of_files = os.listdir(os.getcwd())
-    
+    print(type(list_of_files))
+    print(type(list_of_files[0]))
+
+    manifest ={}
+    for file in list_of_files:
+        name = file.split('.')[0]
+        extention = file.split('.')[-1]
+        print(extention)
+
+        if extention == "txt":
+            manifest.update({name: file})
+    print(manifest["temp"])
+
+    with open("manifest.json", "w") as file:
+        file.write(json.dumps(manifest))
+
+
+if __name__ == "__main__":
+    create_module_manifest()
