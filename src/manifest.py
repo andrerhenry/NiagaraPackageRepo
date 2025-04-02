d4e4f0fab4467109f15d7045b256b5fa044d55e1
andrerhenry <kinghenry238@gmail.com>
2025-04-01 22:35:34 -0700

    adjusted code to read niagara packages

--- src/manifest.py
+++ src/manifest.py
@@ -4,13 +4,15 @@ import json
 def create_module_manifest():
     list_of_files = os.listdir(os.getcwd())
     
+    
     manifest ={}
     for file in list_of_files:
         extension = file.split('.')[-1]
         module_type = (file.split('.')[0]).split('-')[-1]
         name = (file.split('.')[0]).split('-')[0]
-
-        print(name, module_type, extension)
+        extension = file.split('.')[-1]
+        module_type = (file.split('.')[0]).split('-')[-1]
+        name = (file.split('.')[0]).split('-')[0]
 
         if extension == "jar":
             info = {"file": file, "extention": extension}
@@ -18,8 +20,11 @@ def create_module_manifest():
 
 
     with open("manifest.json", "w") as file:
+        manifest = dict(sorted(manifest.items()))
         file.write(json.dumps(manifest))
 
+
+def read_module_manifest():
 
 def read_module_manifest():
     with open("manifest.json", "r") as file:
