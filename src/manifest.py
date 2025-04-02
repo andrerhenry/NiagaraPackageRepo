59982eef6ade88fd8ebc2ec0f53310e5fd49076d
andrerhenry <kinghenry238@gmail.com>
2025-04-01 22:13:55 -0700

    save to json in correct format, check with temp load func

--- src/manifest.py
+++ src/manifest.py
@@ -3,21 +3,29 @@ import json
 
 def create_module_manifest():
     list_of_files = os.listdir(os.getcwd())
-    print(type(list_of_files))
-    print(type(list_of_files[0]))
+
 
     manifest ={}
+    #manifest["info"] = []
     for file in list_of_files:
         name = file.split('.')[0]
         extention = file.split('.')[-1]
-        print(extention)
 
         if extention == "txt":
-            manifest.update({name: file})
-    print(manifest["temp"])
+            info = {"file": file, "extention": extention}
+            manifest[name] = {"file": file, "extention": extention}
+            print(manifest)
 
     with open("manifest.json", "w") as file:
         file.write(json.dumps(manifest))
+        print(type(json.dumps(manifest)))
+
+    with open("manifest.json", "r") as file:
+
+        data = json.loads(file.read())
+        print(type(data))
+        print(data)
+
 
 
 if __name__ == "__main__":
