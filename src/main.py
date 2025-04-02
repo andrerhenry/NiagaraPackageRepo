a0715236ca14008e464199e1605aaa74f54776a8
andrerhenry <kinghenry238@gmail.com>
2025-04-01 22:40:31 -0700

    added logging

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ app = Flask(__name__)
 
 @app.route('/niagara/4.14/<package_name>', methods=['GET'])
 def get_package(package_name:str):
+    app.logger.debug('Get request received, attemping to install %s', package_name)
     return print(f'Attempting to get: {package_name}')
 
 
